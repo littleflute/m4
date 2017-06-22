8ae45cd0e5bb7428c5c50ae697e7ac091f95cb40
littleflute <13699175164@sina.cn>
2017-06-22 16:00:06 -0500

    Add files via upload

---
+++ Sly - the Family Stone/Greatest Hits [Epic]/m.js
@@ -0,0 +1,14 @@
+var f = [];				
+f[0]	= "v0.0.8";			
+f[1]	= "01 I Want to Take You Higher.mp3";		
+f[2]	= "02 Everybody Is a Star.mp3";		
+f[3]	= "03 Stand.mp3";		
+f[4]	= "04 Life.mp3";		
+f[5]	= "05 Fun.mp3";		
+f[6]	= "06 You Can Make It If You Try.mp3";		
+f[7]	= "07 Dance to the Music.mp3";		
+f[8]	= "08 Everyday People.mp3";		
+f[9]	= "09 Hot Fun in the Summertime.mp3";		
+f[10]	= "10 M'Lady.mp3";		
+f[11]	= "11 Sing a Simple Song.mp3";		
+f[12]	= "12 Thank You (Falettinme Be Mice Elf Agin).mp3";		
