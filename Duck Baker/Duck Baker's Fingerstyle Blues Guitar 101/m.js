826b1fd394e96f647c023344dfcaa51b8cfbe135
littleflute <13699175164@sina.cn>
2017-06-22 15:26:36 -0500

    Add files via upload

---
+++ Duck Baker/Duck Baker's Fingerstyle Blues Guitar 101/m.js
@@ -0,0 +1,14 @@
+var f = [];				
+f[0]	= "v0.0.8";			
+f[1]	= "01 Blues in E _1.mp3";		
+f[2]	= "02 Blues in E _2.mp3";		
+f[3]	= "03 Baby Let Me Follow You Down.mp3";		
+f[4]	= "04 BLues in A.mp3";		
+f[5]	= "05 The Jackson Stomp.mp3";		
+f[6]	= "06 Make Me A Pallet On Your Floor.mp3";		
+f[7]	= "07 Sister Kate.mp3";		
+f[8]	= "08 Seven Point One.mp3";		
+f[9]	= "09 Still Staggerin.mp3";		
+f[10]	= "10 The Dirtman Cometh.mp3";		
+f[11]	= "11 The Deep Blue C.mp3";		
+f[12]	= "12 The Mighty Midget.mp3";		
